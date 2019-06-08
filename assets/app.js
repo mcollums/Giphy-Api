40ef7fbb6c15f7e2834294cdd72bdcf5a48c70c6
mcollums <collumsmichelle@gmail.com>
2019-06-08 15:12:55 -0500

    Initial Commit
    Includes some psuedocode in the JS file and general wireframing in the html

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+// When page loads...
+// Load pre-made buttons from an array of strings
+// User can add their own keyword to the array by typing it in the input box and pressing enter
+// When the user presses a button...
+    // We use the giphy api to take that keyword...
+    // Search for giphy results and display the top 10 to the page
+    // Under each gif, it's rating is displayed and any other important information
+// When a user presses a gif, it plays. 
+    // If the gif is playing and the user presses it, it stops.
+
+// Optional: Add more gifs to the page
+    // When user clicks "see next 10 results"
+        //Add more giphy cards to the page
+        //Does not overwrite the current 10
+
+//Optional: Allow users to add gifs to a favorites section
+    //Where will this be displayed?
+    
